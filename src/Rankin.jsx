0a439df5d75452c21f782f1daff261a8f7bb762f
Saif uddin Babar <mdbabar4202@gmail.com>
2026-05-04 22:41:17 +0600

    sdfs

--- src/Rankin.jsx
+++ src/Rankin.jsx
@@ -13,14 +13,100 @@ function Ranking() {
       .then(data => setData(data));
   }, [code]);
 
+  if (!data.length) return <h2 style={{ textAlign: "center" }}>No Data</h2>;
+
+  const top3 = data.slice(0, 3);
+  const rest = data.slice(3);
+
   return (
-    <div>
-      <h2>Ranking</h2>
-      {data.map((item, i) => (
-        <div key={i}>
-          #{i + 1} {item.name} - {item.score}
-        </div>
-      ))}
+    <div style={{
+      padding: 20,
+      maxWidth: 500,
+      margin: "auto",
+      background: "#064e3b",
+      minHeight: "100vh",
+      color: "white"
+    }}>
+
+      <h2 style={{ textAlign: "center" }}>🏆 Ranking</h2>
+
+      {/* TOP 3 */}
+      <div style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "end",
+        marginTop: 30
+      }}>
+
+        {/* 2nd */}
+        {top3[1] && (
+          <div style={{ textAlign: "center" }}>
+            <div style={{
+              width: 70,
+              height: 70,
+              borderRadius: "50%",
+              background: "#fff",
+              margin: "auto",
+              border: "4px solid orange"
+            }} />
+            <p>{top3[1].name}</p>
+            <p>{top3[1].score} pts</p>
+          </div>
+        )}
+
+        {/* 1st */}
+        {top3[0] && (
+          <div style={{ textAlign: "center" }}>
+            <div style={{
+              width: 90,
+              height: 90,
+              borderRadius: "50%",
+              background: "#fff",
+              margin: "auto",
+              border: "5px solid gold"
+            }} />
+            <h3>👑 {top3[0].name}</h3>
+            <p>{top3[0].score} pts</p>
+          </div>
+        )}
+
+        {/* 3rd */}
+        {top3[2] && (
+          <div style={{ textAlign: "center" }}>
+            <div style={{
+              width: 70,
+              height: 70,
+              borderRadius: "50%",
+              background: "#fff",
+              margin: "auto",
+              border: "4px solid bronze"
+            }} />
+            <p>{top3[2].name}</p>
+            <p>{top3[2].score} pts</p>
+          </div>
+        )}
+      </div>
+
+      {/* LIST */}
+      <div style={{ marginTop: 30 }}>
+        {rest.map((item, index) => (
+          <div
+            key={index}
+            style={{
+              padding: 12,
+              marginTop: 10,
+              borderRadius: 10,
+              background: "#065f46",
+              display: "flex",
+              justifyContent: "space-between"
+            }}
+          >
+            <span>#{index + 4} {item.name}</span>
+            <span>{item.score} pts</span>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
