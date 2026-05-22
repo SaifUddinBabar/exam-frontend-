f76bc8212dbf20f35ddfec90f7afd1e9a738d784
Saif uddin Babar <mdbabar4202@gmail.com>
2026-05-22 10:17:48 +0600

    feat: PWA added

--- src/main.jsx
+++ src/main.jsx
@@ -9,3 +9,17 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </BrowserRouter>
 );
+
+// ✅ PWA Service Worker Register
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => {
+        console.log("✅ Service Worker Registered");
+      })
+      .catch((err) => {
+        console.log("❌ SW Registration Failed:", err);
+      });
+  });
+}
