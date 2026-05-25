6418fc5ec7028b6e3c0c518d752fdd891f486922
Saif uddin Babar <mdbabar4202@gmail.com>
2026-05-25 21:24:59 +0600

    add board

--- src/Builder.jsx
+++ src/Builder.jsx
@@ -1142,6 +1142,8 @@ function Builder() {
                       <option value="Rajshahi">Rajshahi</option>
                       <option value="Cumilla">Cumilla</option>
                       <option value="Jessore">Jessore</option>
+                      <option value="Dinajpur">Dinajpur</option>
+                      <option value="Barishal">Barishal</option>
                     </select>
  
                   </div>
