6fef6ca0104d5fbdbee493b209bf3afe44a86aaa
Saif uddin Babar <mdbabar4202@gmail.com>
2026-05-25 22:07:39 +0600

    add feature

--- src/Builder.jsx
+++ src/Builder.jsx
@@ -1142,7 +1142,6 @@ function Builder() {
                       <option value="Rajshahi">Rajshahi</option>
                       <option value="Cumilla">Cumilla</option>
                       <option value="Jessore">Jessore</option>
-                      <option value="Dinajpur">Dinajpur</option>
                     </select>
  
                   </div>
